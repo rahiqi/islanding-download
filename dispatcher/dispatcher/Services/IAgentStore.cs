9abac291ef1f8a06e3c8d8ff94a76b2add31fd20
Ali Rahighi <rahighi@hotmail.com>
2026-04-01 11:37:59 +0330

    fix errors

--- dispatcher/dispatcher/Services/IAgentStore.cs
+++ dispatcher/dispatcher/Services/IAgentStore.cs
@@ -3,6 +3,6 @@ namespace dispatcher.Services;
 public interface IAgentStore
 {
     void RegisterAgent(string agentId, string name, string location);
-    void UpsertAgent(string agentId, DateTime lastSeen, int currentDownloads);
+    void UpsertAgent(string agentId, DateTime lastSeen, int currentDownloads, long? totalBytes = null, long? freeBytes = null, long? usedBytes = null);
     IReadOnlyList<dispatcher.Models.AgentInfo> GetAvailableAgents(TimeSpan maxAge);
 }
